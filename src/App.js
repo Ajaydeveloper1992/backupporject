8c62a1a51d4537332e0b707a950cffc2e945a713
Ajaydeveloper1992 <ajay.shriwas1992@gmail.com>
2024-08-12 16:22:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import Dashboard from '/components/Dashboard';
+
+const App = () => {
+    return (
+        <div>
+            <h2 className='app-title'>Job Place App</h2>
+            <hr />
+            This is new topice and value
+        </div>
+     );
+}
+export default App;
